569a3ed0b6bb82ba5c21268fbc573ed5ded2f545
James Quick <james.q.quick@outlook.com>
2020-10-05 07:56:13 -0500

    Copy of podia site ready...I think :)

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <div className="flex items-center justify-center w-screen h-screen p-4">
-            <Component {...pageProps} />
-        </div>
-    );
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                brand: '#de5254',
+                'brand-50': '#FDF6F6',
+                'brand-100': '#FCEEEE',
+                'brand-200': '#F7D4D4',
+                'brand-300': '#F2BABB',
+                'brand-400': '#E88687',
+                brand: '#DE5254',
+                'brand-600': '#C84A4C',
+                'brand-700': '#853132',
+                'brand-800': '#642526',
+                'brand-900': '#431919',
             },
         },
     },
